a077e919277efb2f81079090f181bbe9ea76293c
Samuel Brown <samuelbrown3199@gmail.com>
2021-11-14 01:10:58 +0000

    Update main.cpp

--- src/TestProgram/main.cpp
+++ src/TestProgram/main.cpp
@@ -17,37 +17,9 @@ int main()
 {
 	std::shared_ptr<Application> app = Application::InitializeEngine("Test Program", GraphicsAPI::OpenGL, 1366, 768);
 
-	std::shared_ptr<Shader> vertexShader = ResourceManager::LoadResource<Shader>("Resources\\Shaders\\standard.vs");
-	std::shared_ptr<Shader> fragmentShader = ResourceManager::LoadResource<Shader>("Resources\\Shaders\\standard.fs");
-
-	std::shared_ptr<ShaderProgram> temp = ResourceManager::CreateShaderProgram();
-	temp->BindShader(vertexShader, GL_VERTEX_SHADER);
-	temp->BindShader(fragmentShader, GL_FRAGMENT_SHADER);
-	temp->LinkShaderProgram(temp);
-
-	std::shared_ptr<Texture> testTex = ResourceManager::LoadResource<Texture>("Resources\\Textures\\test.png");
-
 	Entity* test = Application::CreateEntity();
 	test->AddComponent<Transform>(glm::vec3(0, 0, 5), glm::vec3(0,0,-1));
-	test->AddComponent<Camera>(true, CameraMode::projection);
-	test->AddComponent<FlyingCam>();
-
-	int amount = 5;
-	for (int x = 0; x < amount; x++)
-	{
-		for (int y = 0; y < amount; y++)
-		{
-			for (int z = 0; z < amount; z++)
-			{
-				Entity* renderTest = Application::CreateEntity();
-				renderTest->AddComponent<PhysicsBody>(box, 10, glm::vec3(0 + (x * 10), 5 + (y * 10), -10 +(z*10)), glm::vec3(40, 25, 73), glm::vec3(1, 1, 1));
-				renderTest->AddComponent<Mesh>(Physics, ResourceManager::LoadResource<Model>("Resources\\Models\\cube.obj"), temp, testTex);
-			}
-		}
-	}
-	Entity* floor = Application::CreateEntity();
-	floor->AddComponent<PhysicsBody>(box, 0, glm::vec3(0 , 0 , 0), glm::vec3(0, 0, 0), glm::vec3(1000, 1, 1000));
-	floor->AddComponent<Mesh>(Physics, ResourceManager::LoadResource<Model>("Resources\\Models\\cube.obj"), temp, testTex);
+	//test->AddComponent<Camera>(true, CameraMode::projection);
 
 	app->BindSystem<FlyingCamSystem>(SystemUsage::useUpdate);
 	app->MainEngineLoop();
